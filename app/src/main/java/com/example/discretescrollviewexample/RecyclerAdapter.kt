cc3b8c4b2c8d81d34704db596c145f85c2863c6a
GeonwooVincentKim <kdsnop@gmail.com>
2021-10-04 15:21:24 +0900

    [Feature] - Added RecyclerAdapter project

---
+++ app/src/main/java/com/example/discretescrollviewexample/RecyclerAdapter.kt
@@ -0,0 +1,68 @@
+package com.example.discretescrollviewexample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+    private var titles = arrayOf(
+        "Chapter One",
+        "Chapter Two",
+        "Chapter Three",
+        "Chapter Four",
+        "Chapter Five",
+        "Chapter Six",
+        "Chapter Seven",
+        "Chapter Eight"
+    )
+
+    private var details = arrayOf(
+        "Item one details",
+        "Item two details",
+        "Item three details",
+        "Item four details",
+        "Item five details",
+        "Item six details",
+        "Item seven details",
+        "Item eight details"
+    )
+
+    private var images = intArrayOf(
+        R.drawable.ic_kotlin_logo,
+        R.drawable.ic_kotlin_logo,
+        R.drawable.ic_kotlin_logo,
+        R.drawable.ic_kotlin_logo,
+        R.drawable.ic_kotlin_logo,
+        R.drawable.ic_kotlin_logo,
+        R.drawable.ic_kotlin_logo,
+        R.drawable.ic_kotlin_logo
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemDetail = itemView.findViewById(R.id.item_detail)
+        }
+    }
+}
