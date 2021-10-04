567bc72a713a6dbe5cf68fc3d74dcbe2bee8b905
GeonwooVincentKim <kdsnop@gmail.com>
2021-10-04 15:45:11 +0900

    [Feature] - Implemented RecyclerViewExample project

--- app/src/main/java/com/example/discretescrollviewexample/MainActivity.kt
+++ app/src/main/java/com/example/discretescrollviewexample/MainActivity.kt
@@ -9,11 +9,18 @@ class MainActivity : AppCompatActivity() {
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
+    private val recyclerViewExample: RecyclerView by lazy {
+        findViewById(R.id.recyclerViewExample)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         layoutManager = LinearLayoutManager(this)
+        recyclerViewExample.layoutManager = layoutManager
 
+        adapter = RecyclerAdapter()
+        recyclerViewExample.adapter = adapter
     }
 }
